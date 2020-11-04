8c159564096eeb720ecbcbd01e9a4ce796701849
Neil Smyth <neil@thesmyths.eu>
2020-11-04 20:27:56 +0100

    updated sample data loading setup

--- src/submit-sample-data.ts
+++ src/submit-sample-data.ts
@@ -1,23 +1,52 @@
 import { CherrytwistClient } from 'cherrytwist-lib';
+import { EcoversePopulator } from './EcoversePopulator';
 
 const main = async () => {
+  require("dotenv").config();
 
-  //const endpoint = 'http://dev.cherrytwist.org/graphql'
-  const endpoint = 'http://localhost:4000/graphql'
-  const ctClient = new CherrytwistClient(endpoint);
+  ////////// First connect to the ecoverse //////////////////
+  const endPoint = process.env.CT_SERVER;
+  if (!endPoint) throw new Error("CT_SERVER enironment variable not set");
 
-  // Test can connect to the server
-  const serverAvailable = await ctClient.testConnection();
-  if (!serverAvailable) {
-    throw new Error("Unable to connect, aborting...");
-  }
+  const populator = new EcoversePopulator(endPoint);
+  // Get an authorisation token
+  populator.logger.info(`Cherrytwist server: ${endPoint}`);
 
-  // Load in mutations file 
-  const mutationFile = "./src/sample-ecoverse.json";
-  const result = await ctClient.submitMultipleMutations(mutationFile);
-  if (result) {
-    console.log(`==> Loading data completed successfully!`);
+  // Update the context and set the host
+  await populator.updateEcoverseContext();
+  await populator.updateHostOrganisation();
+  await createGroups(populator);
+
+  const userVariablesDir = "./src/data/users";
+  await populator.createUsers(userVariablesDir);
+
+  const challengesVariablesDir = "./src/data/challenges";
+  await populator.createChallenges(challengesVariablesDir);
+}
+
+// Load in mutations file
+async function createGroups(populator: EcoversePopulator) {
+  const groups = [
+    "Leads",
+    "Team Members",
+    "Specialists",
+    "Crew",
+    "Challenge Leads",
+  ];
+  populator.logger.info(
+    `===================================================================`
+  );
+
+  populator.logger.info(`To create ${groups.length} ecoverse groups`);
+  // Iterate over the rows
+  for (let i = 0; i < groups.length; i++) {
+    const groupName = groups[i];
+    await populator.createEcoverseGroup(groupName);
   }
 }
 
-main().catch((error) => console.error(error));
+try {
+  main();
+} catch (error) {
+  console.error(error);
+}
