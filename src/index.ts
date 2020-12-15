b20505cabe1b99f0b53d59b32aec68d2723dd0e7
Angel <atstojanov@gmail.com>
2020-12-15 16:45:38 +0200

    Add link to the official README.md

--- src/index.ts
+++ src/index.ts
@@ -13,26 +13,51 @@ const main = async () => {
 
   program.on('--help', () => {
     console.log();
-    console.log(`cherrytwist-demo populate -s `);
+    console.log(
+      `For more information visit: https://github.com/cherrytwist/Demo/blob/develop/README.md`
+    );
   });
 
   program
     .command('populate')
     .alias('p')
-    .option('-f, --file <fileName>', `ODS/XLSX file to import, if not set sample-data will be populated`)
-    .option('-s, --server <url>', 'cherry twist graphql endpoint', 'http://localhost:4000/graphql')
+    .option(
+      '-f, --file <fileName>',
+      `ODS/XLSX file to import, if not set sample-data will be populated`
+    )
+    .option(
+      '-s, --server <url>',
+      'cherry twist graphql endpoint',
+      'http://localhost:4000/graphql'
+    )
     .description('import data from ODS/XLSX file to cherry twist server')
     .action(opts => {
       sampleData(opts.file, opts.server);
+    })
+    .on('--help', () => {
+      console.log();
+      console.log(
+        `For more information visit: https://github.com/cherrytwist/Demo/blob/develop/README.md`
+      );
     });
 
   program
     .command('populate-avatars')
     .alias('a')
-    .option('-s, --server <url>', 'cherry twist graphql endpoint', 'http://localhost:4000/graphql')
+    .option(
+      '-s, --server <url>',
+      'cherry twist graphql endpoint',
+      'http://localhost:4000/graphql'
+    )
     .description('populate avatars into destination server')
     .action(opts => {
       populateAvatars(opts.server);
+    })
+    .on('--help', () => {
+      console.log();
+      console.log(
+        `For more information visit: https://github.com/cherrytwist/Demo/blob/develop/README.md`
+      );
     });
 
   await program.parseAsync(process.argv);
