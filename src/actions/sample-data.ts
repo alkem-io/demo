1b546ac030424fd3565671d40de26483f2f2e5a6
Neil Smyth <neil@thesmyths.eu>
2021-03-24 12:33:32 +0100

    fixed sample data loading to work with new populator setup

--- src/actions/sample-data.ts
+++ src/actions/sample-data.ts
@@ -1,28 +1,35 @@
-import { CherrytwistClient } from 'cherrytwist-lib';
+import { CherrytwistClient } from '@cherrytwist/client-lib';
 import {
   XLSXAdapter,
   Populator,
   createLogger,
   createProfiler,
-} from 'cherrytwist-populator';
-import { assert } from 'console';
+} from '@cherrytwist/populator';
+import * as dotenv from 'dotenv';
 import path from 'path';
 
-export const sampleData = async (fileName: string, server: string) => {
-  assert(server, 'Server url not provided!');
-
+export const sampleData = async () => {
+  dotenv.config();
   const logger = createLogger();
   const profiler = createProfiler();
 
+  const server = process.env.CT_SERVER || 'http://localhost:4000/graphql';
+  const dataTemplate =
+    process.env.CT_DATA_TEMPLATE || '../cherrytwist-data-template.ods';
+  const accessToken = process.env.CT_ACCESS_TOKEN || 'eyNotSet';
   const ctClient = new CherrytwistClient({
     graphqlEndpoint: server,
+    accessToken: `${accessToken}`,
   });
 
   logger.info(`Cherrytwist server: ${server}`);
+  logger.info(`Cherrytwist data template: ${dataTemplate}`);
 
-  const data = new XLSXAdapter(fileName);
+  await ctClient.validateConnection();
 
+  const data = new XLSXAdapter(path.join(__dirname, '..', dataTemplate));
   // Loading data from google sheets
   const populator = new Populator(ctClient, data, logger, profiler);
   await populator.populate();
 };
+
