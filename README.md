fe212f9fd9b1368ce23f154b325cacd0a8d25176
Neil Smyth <neil@thesmyths.eu>
2021-06-21 10:54:30 +0200

    updated packages; server running, client cannot connect

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ At this point you hopefully will have a running empty ecoverse! Both client and 
 Once the cluster is setup, the next step is to load data into the Cherrytwist Server:
 * **Setup environment variables**: Copy the `.env.default` file to be `.env` file
 * **Install package dependencies**: From the command line execute the following command to install all required package dependencies: `npm install`
-* **Populate with sample data**: From the commang line, execute the following commange to populate the server with data: `npm run sample-data`.
+* **Populate with sample data**: From the command line, execute the following commange to populate the server with data: `npm run sample-data`.
   * Note: this can take a couple of minutes, you will see the progress in the window where you executed the command.
 
 At this point you hopefully have a populated Ecoverse, with Challenges / sample users visible.
--- src/actions/sample-data.ts
+++ src/actions/sample-data.ts
@@ -1,4 +1,4 @@
-import { CherrytwistClient } from '@cherrytwist/client-lib';
+import { AuthInfo, CherrytwistClient } from '@cherrytwist/client-lib';
 import {
   XLSXAdapter,
   Populator,
@@ -16,20 +16,46 @@ export const sampleData = async () => {
   const server = process.env.CT_SERVER || 'http://localhost:4000/graphql';
   const dataTemplate =
     process.env.CT_DATA_TEMPLATE || '../cherrytwist-data-template.ods';
-  const accessToken = process.env.CT_ACCESS_TOKEN || 'eyNotSet';
+  const authInfo = await getAuthInfo();
   const ctClient = new CherrytwistClient({
     graphqlEndpoint: server,
-    accessToken: `${accessToken}`,
+    authInfo: authInfo
   });
 
   logger.info(`Cherrytwist server: ${server}`);
   logger.info(`Cherrytwist data template: ${dataTemplate}`);
 
   await ctClient.validateConnection();
+  const ecoverseID = 'cherrytwist';
+  const ecoverseHostID = 'cherrytwistOrg';
+  const ecoverseExists = await ctClient.ecoverseExists(ecoverseID);
+  console.log(`Ecoverse '${ecoverseID}' exists: ${ecoverseExists}`);
+  if (!ecoverseExists) {
+    console.log(`Creating '${ecoverseID}' ecoverse and '${ecoverseHostID}' host organisaiton.`);
+  // create host org
+    await ctClient.createOrganisation(ecoverseHostID, ecoverseHostID);
+    await ctClient.createEcoverse({
+      nameID: ecoverseID,
+      displayName: ecoverseID,
+      hostID: ecoverseHostID
+    })
+  }
 
   const data = new XLSXAdapter(path.join(__dirname, '..', dataTemplate));
   // Loading data from google sheets
   const populator = new Populator(ctClient, data, logger, profiler);
   await populator.populate();
 };
+
+async function getAuthInfo(): Promise<AuthInfo | undefined> {
+  return {
+    credentials: {
+      email: process.env.AUTH_ADMIN_EMAIL ?? 'admin@cherrytwist.org',
+      password: process.env.AUTH_ADMIN_PASSWORD ?? '!Rn5Ez5FuuyUNc!',
+    },
+    apiEndpointFactory: () => {
+      return 'http://localhost:4433/';
+    },
+  };
+}
 
